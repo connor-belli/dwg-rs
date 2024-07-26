9ff21bf1a11e81c2bdc65a16f912104d90b41f7a
Connor Belli <connorbelli2003@gmail.com>
2024-07-25 20:38:07 -0400

    Added support for reading bitshorts, bitdoubles and modular chars

--- src/bitcodes.rs
+++ src/bitcodes.rs
@@ -1,5 +1,7 @@
 use std::mem::size_of;
 
+use arrayvec::ArrayVec;
+
 pub struct BitReader<'a, I: Iterator<Item = &'a u8>> {
     cur_byte: u8,
     cur_bit: usize,
@@ -60,6 +62,51 @@ impl<'a, I: Iterator<Item = &'a u8>> BitReader<'a, I> {
     pub fn read_bit(&mut self) -> Option<u8> {
         self.read_bits::<1>().map(|x| x as u8)
     }
+
+    pub fn read_bitshort(&mut self) -> Option<i16> {
+        let flag = self.read_bits::<2>()?;
+        match flag {
+            0x0 => self.read_bits::<16>().map(|x| x as i16),
+            0x1 => self.read_bits::<8>().map(|x| x as i16),
+            0x2 => Some(0),
+            0x3 => Some(256),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn read_bitdouble(&mut self) -> Option<f64> {
+        let flag = self.read_bits::<2>()?;
+        match flag {
+            0x0 => {
+                // TODO: optimize
+                let mut bytes = [0; 8];
+                for byte in &mut bytes {
+                    *byte = self.read_bits::<8>()? as u8;
+                }
+                Some(f64::from_be_bytes(bytes))
+            }
+            0x1 => Some(1.0),
+            0x2 => Some(0.0),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn read_modular_char(&mut self) -> Option<i32> {
+        // TODO: we don't need to use an arrayvec here
+        let mut bytes: ArrayVec<u8, 8> = ArrayVec::new();
+        loop {
+            let byte = self.read_bits::<8>()? as u8; 
+            bytes.push(byte & !(1 << 7));
+            if byte & (1 << 7) == 0 {
+                break;
+            }
+        }
+        let mut res = 0i32;
+        for (i, byte) in bytes.into_iter().enumerate() {
+            res |= (byte as i32) << (i*7); 
+        }
+        Some(res)
+    }
 }
 
 #[test]
@@ -71,4 +118,12 @@ fn test_read_bits() {
     assert_eq!(reader.read_bits::<5>(), Some(0x1B));
     assert_eq!(reader.read_bits::<3>(), Some(0x5));
     assert_eq!(reader.read_bits::<1>(), None);
+}
+
+#[test]
+fn test_read_modular_char() {
+    // Opendesign specification example
+    let buf: [_; 2] = [0b10000010, 0b00100100];
+    let mut reader = BitReader::new(buf.iter());
+    assert_eq!(reader.read_modular_char(), Some(4610));
 }
